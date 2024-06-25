d2bad2f4de3de716b9e34644a3dd228a25001668
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-06-25 08:57:30 -0400

    This should work

--- mods.js
+++ mods.js
@@ -4,7 +4,6 @@ function doSomething() {
     if (typeof (element) != 'undefined' && element != null) {
 Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
 Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js');
-Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js');
 Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js');
 Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/Horticookie.js');
     } else {
