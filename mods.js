5810265e9aae2ce4037e5a26675e3c526a521e82
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-06-25 11:29:14 -0400

    OOOOOOOHHHHHH

--- mods.js
+++ mods.js
@@ -2,11 +2,11 @@ setTimeout(doSomething, 3000);
 function doSomething() {
     var element = document.getElementById('particle0');
     if (typeof (element) != 'undefined' && element != null) {
- Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
-Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
-Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js');
 Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js');
 Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/Horticookie.js');
+Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
+Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
+Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js');
     } else {
         setTimeout(doSomething, 3000);
     }
