5c5e946a9c74d3181f574e6399a6bc9d7b63b45e
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-06-25 10:09:27 -0400

    Larger timeout

--- mods.js
+++ mods.js
@@ -1,13 +1,13 @@
-setTimeout(doSomething, 1000);
+setTimeout(doSomething, 3000);
 function doSomething() {
     var element = document.getElementById('particle0');
     if (typeof (element) != 'undefined' && element != null) {
+ Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
 Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
 Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js');
 Game.LoadMod('https://rawgit.com/yannprada/cookie-garden-helper/master/cookie-garden-helper.js');
 Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/Horticookie.js');
-Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
     } else {
-        setTimeout(doSomething, 1000);
+        setTimeout(doSomething, 3000);
     }
 }
