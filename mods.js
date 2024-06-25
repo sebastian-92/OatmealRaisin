a4cc1c6da6d2401eb4253e44823cc267b47de178
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-06-25 11:37:03 -0400

    Insugar trading

--- mods.js
+++ mods.js
@@ -7,6 +7,7 @@ Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/Horticookie.js
 Game.LoadMod("https://icehawk78.github.io/FrozenCookies/frozen_cookies.js");
 Game.LoadMod("https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js");
 Game.LoadMod('https://cdn.jsdelivr.net/gh/Ancyker/Crustulum/Crustulum.js');
+Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js');
     } else {
         setTimeout(doSomething, 3000);
     }
