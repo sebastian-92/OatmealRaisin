a4471e2748e75076ca035786024d33449e19a6e5
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-07-16 21:19:53 -0400

    jsdelivr

--- mods.js
+++ mods.js
@@ -2,19 +2,19 @@
 setTimeout(doSomething, 500);
 function doSomething() {
     if (Game.ready) {
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE-POCs/BlackholeInverter.js?v=1.14');
-        Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js');
-        Game.LoadMod('https://staticvariablejames.github.io/SpicedCookies/Spice.js');
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/Horticookie.js');
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/AmericanSeason.js');
-        Game.LoadMod('https://klattmose.github.io/CookieClicker/minigameCasino.js');
-        Game.LoadMod("https://klattmose.github.io/CookieClicker/IdleTrading.js?v=1.10");
-        Game.LoadMod('https://hyoretsu.github.io/DarkysCheevosPackage/dist/main.js');
-        Game.LoadMod('https://gamrguy.github.io/OmniscientCookies/OmniscientCookies.js');
-        Game.LoadMod("https://cbass92.org/OatmealRaisin/frozenCookiesCustom.js");
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/CCSE-POCs/BlackholeInvertermin.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/staticvariablejames/InsugarTrading/InsugarTrading.min.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/staticvariablejames/SpicedCookies/Spice.min.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/Horticookie.min.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/AmericanSeason.min.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/minigameCasino.min.js');
+        Game.LoadMod("https://cdn.jsdelivr.net/gh/klattmose/CookieClicker/IdleTrading.min.js");
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/hyoretsu/DarkysCheevosPackage/dist/main.min.js');
+        Game.LoadMod('https://cdn.jsdelivr.net/gh/gamrguy/OmniscientCookies/OmniscientCookies.min.js');
+        Game.LoadMod("https://cdn.jsdelivr.net/gh/cdn.jsdelivr.net/gh/sebastian-92/OatmealRaisin/frozenCookiesCustom.min.js");
         var scriptII=document.createElement('script');
         scriptII.setAttribute('type','text/javascript');
-        scriptII.setAttribute('src','https://worldwidewaves.github.io/Cookie-Stonks/main.user.js');
+        scriptII.setAttribute('src','https://cdn.jsdelivr.net/gh/worldwidewaves/Cookie-Stonks/main.user.js');
         document.body.appendChild(scriptII);
         Game.Win("Not Quite Chocolate Chip");
     } else {
